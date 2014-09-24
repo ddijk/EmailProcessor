bdbba8780178d0dfc6708ac6824f963e16816463
Dick Dijk <dickdijk@gmail.com>
2014-09-24 14:26:25 +0200

    removed account info

--- src/main/java/nl/dijkrosoft/snippets/email/sponsorzwemmen/ReadingEmailMultpart.java
+++ src/main/java/nl/dijkrosoft/snippets/email/sponsorzwemmen/ReadingEmailMultpart.java
@@ -1,18 +1,17 @@
 package nl.dijkrosoft.snippets.email.sponsorzwemmen;
 
-
-       import java.util.*;
-import javax.mail.*;
+import java.util.Properties;
 
 public class ReadingEmailMultpart {
+
     public static void main(String[] args) {
         Properties props = new Properties();
         props.setProperty("mail.store.protocol", "imaps");
         try {
             Session session = Session.getInstance(props, null);
             Store store = session.getStore();
-           store.connect("imap.xs4all.nl", "dickdijk", "aragon95");
-	
+            store.connect("imap.xs4all.nl", "**user**", "**wachtw**");
+
             Folder inbox = store.getFolder("INBOX");
             inbox.open(Folder.READ_ONLY);
             Message msg = inbox.getMessage(inbox.getMessageCount());
